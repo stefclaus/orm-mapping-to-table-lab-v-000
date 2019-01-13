4931b065ae48d3d2f5ea40895bbf49a87e6052b0
Stef Claus <stef.claus@gmail.com>
2019-01-13 01:25:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,10 +39,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name:, album:)
-    song = Song.new(name, album)
-    song.save
-    song
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
     end
